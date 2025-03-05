9088e85f5fef8dc6eb822eb6dbd2a84c31e947c3
Dominik Klotz <105296959+programminx-askui@users.noreply.github.com>
2025-03-05 11:10:08 +0100

    chore: fix misspelling

--- src/pytest-qa-agent/tests/test_my_test.py
+++ src/pytest-qa-agent/tests/test_my_test.py
@@ -3,6 +3,6 @@ from askui import VisionAgent
 
 def test_my_test(agent: VisionAgent):
     agent.click("Hello World")
-    anser = agent.get("Is the mouse over 'Hello World'? Answer with 'yes' or 'no'.")
+    answer = agent.get("Is the mouse over 'Hello World'? Answer with 'yes' or 'no'.")
 
-    assert anser == "yes"
+    assert answer == "yes"
